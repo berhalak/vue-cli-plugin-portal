328ecb4444023a407b443602c6f3fc66b26b3791
berhalak <jaroslaw.sadzinski@gmail.com>
2020-03-03 00:36:47 +0100

    Upgrade

--- dist/loader.js
+++ dist/loader.js
@@ -1,6 +1,11 @@
 "use strict";
 var code = require('./code');
 module.exports = function (source) {
+    var _a, _b;
+    var vue = (_b = (_a = this === null || this === void 0 ? void 0 : this.resource) === null || _a === void 0 ? void 0 : _a.split("?")[0]) !== null && _b !== void 0 ? _b : "";
+    if (vue.includes("node_modules")) {
+        return source;
+    }
     return code.rewrite(source);
 };
 //# sourceMappingURL=loader.js.map
--- loader.ts
+++ loader.ts
@@ -1,5 +1,9 @@
 const code = require('./code');
 
-module.exports = function (source: string) {
+module.exports = function (this: any, source: string) {
+	let vue = this?.resource?.split("?")[0] ?? "";
+	if (vue.includes("node_modules")) {
+		return source;
+	}
 	return code.rewrite(source);
 };
